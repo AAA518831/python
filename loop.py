138c1a8e2b078a83c665ef7a0aa6f378326b4c0b
kse <goy2k327@gmail.com>
2025-07-17 10:15:02 +0900

    파이썬 반복문 실습.

--- loop.py
+++ loop.py
@@ -1,4 +1,8 @@
 # for i in [1,2,3,4,5]: #끝에 :이 있어야 함
 #     print("방문을 환영합니다.") #들여쓰기 해야 함
-for i in [1,2,3,4,5]:
-    print("i=", i)
+# for i in [1,2,3,4,5]:
+#     print("i=", i)
+a=100
+b=200
+print(a,b,sep="와", end="  끝")
+print(a,b, end="  ") #줄바꿈하지 않고 옆으로 출력
