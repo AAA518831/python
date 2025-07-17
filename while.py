2b48744bdfb5aed8ddd727a321e170541b617a65
kse <goy2k327@gmail.com>
2025-07-17 14:45:21 +0900

    파이썬 while문 실습

---
+++ while.py
@@ -0,0 +1,20 @@
+#password=""
+
+#while password !="pythonisfun":
+#    password=input("암호를 입력하시오: ")
+#print("로그인 성공")
+
+#cnt = 1
+#sum = 0
+#while cnt <= 100:
+#    sum=sum+cnt
+ #   cnt=cnt+1
+#cnt=cnt-1
+#print(f"개수는 {cnt} 합계는 {sum}")
+
+num1 = 0
+while num2 <= 4: 
+    num2=int(input("정수를 입력하세요>> "))
+    if num2%2==0:
+        num1=num1+1
+print("짝수 5개가 입력되었습니다")
