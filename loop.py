25e133fabbafbf509bb6cd5134f22c9cdbc9dd6f
kse <goy2k327@gmail.com>
2025-07-17 10:20:39 +0900

    파이썬 반복문 실습.

--- loop.py
+++ loop.py
@@ -2,7 +2,14 @@
 #     print("방문을 환영합니다.") #들여쓰기 해야 함
 # for i in [1,2,3,4,5]:
 #     print("i=", i)
-a=100
-b=200
-print(a,b,sep="와", end="  끝")
-print(a,b, end="  ") #줄바꿈하지 않고 옆으로 출력
+# a=100
+# b=200
+# print(a,b,sep="와", end="  끝")
+# print(a,b, end="  ") #줄바꿈하지 않고 옆으로 출력
+
+import turtle
+t=turtle.Turtle()
+
+for count in range(6):
+    t.circle(100)
+    t.left(360/6)
